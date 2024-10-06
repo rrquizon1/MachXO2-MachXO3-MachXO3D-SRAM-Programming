f8d012b76e0a8262df760cc53ea46feffb415686
rrquizon1 <70574862+rrquizon1@users.noreply.github.com>
2024-10-06 09:26:07 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,63 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <assert.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/ioctl.h>
+#include <linux/spi/spidev.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <gpiod.h>
+#include "spi_data.h"
+#include "spi.h"
+//#include "data.h"
+
+
+
+
+struct gpiod_line *cs;
+struct gpiod_line *prgrmn;
+
+
+
+int main() {
+   
+    spi_init(3000*1024,&cs,&prgrmn);
+    
+    usleep(1000);
+    //Device ID read 	
+    device_id();
+    
+    usleep(1000);
+    //ISC_ENABLE
+    isc_enable();
+    usleep(1000);
+    
+    //ISC_ERASE
+    isc_erase();
+    usleep(1000);
+    
+    //Status Register Check, DONE=0 and Fail=0
+    sr_check(0);
+    
+    fast_program();
+    
+    usleep(1000);
+    
+    
+    //Status Register Check, DONE=1 and Fail=0
+    sr_check(1);
+
+    usleep(1000);
+	
+    //Exits programming mode and enters usermode
+    isc_disable();
+
+    rbpi_exit();
+	
+	
+}
